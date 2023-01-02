76debc2e5287e23a786fe5992f07aa9249997b63
No lifer <davydqq@gmail.com>
2023-01-02 19:01:17 +0200

    init documents && videos

--- EnglishWordsRecognizer/TB.Documents/Commands/HandleDocumentsCommandHandler.cs
+++ EnglishWordsRecognizer/TB.Documents/Commands/HandleDocumentsCommandHandler.cs
@@ -1,5 +1,8 @@
 ﻿using CQRS.Commands;
 using Microsoft.Extensions.Logging;
+using TB.Core.Commands;
+using TB.Localization.Services;
+using Telegram.Bot.Types.Enums;
 
 namespace TB.Documents.Commands;
 
@@ -7,13 +10,24 @@ public class HandleDocumentsCommandHandler : ICommandHandler<HandleDocumentsComm
 {
     private readonly ILogger<HandleDocumentsCommandHandler> logger;
 
-    public HandleDocumentsCommandHandler(ILogger<HandleDocumentsCommandHandler> logger)
+    private readonly ICommandDispatcher commandDispatcher;
+
+    private readonly ILocalizationService localizationService;
+
+    public HandleDocumentsCommandHandler(
+        ILogger<HandleDocumentsCommandHandler> logger,
+        ICommandDispatcher commandDispatcher,
+        ILocalizationService localizationService)
     {
         this.logger = logger;
+        this.commandDispatcher = commandDispatcher;
+        this.localizationService = localizationService;
     }
 
-    public Task HandleAsync(HandleDocumentsCommand command, CancellationToken cancellation = default)
+    public async Task HandleAsync(HandleDocumentsCommand command, CancellationToken cancellation = default)
     {
-        return Task.CompletedTask;
+        var text = await localizationService.GetTranslateByInterface("app.file.noSupportContent", command.UserId);
+        var commandTelegram = new SendMessageCommand(command.ChatId, text, parseMode: ParseMode.Html, replyToMessageId: command.MessageId);
+        await commandDispatcher.DispatchAsync(commandTelegram);
     }
 }
--- EnglishWordsRecognizer/TB.Videos/Commands/HandleVideosCommandHandler.cs
+++ EnglishWordsRecognizer/TB.Videos/Commands/HandleVideosCommandHandler.cs
@@ -1,19 +1,32 @@
 ﻿using CQRS.Commands;
 using Microsoft.Extensions.Logging;
+using TB.Core.Commands;
+using TB.Localization.Services;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace TB.Videos.Commands;
 
 public class HandleVideosCommandHandler : ICommandHandler<HandleVideosCommand>
 {
     private readonly ILogger<HandleVideosCommandHandler> logger;
+    private readonly ICommandDispatcher commandDispatcher;
+    private readonly ILocalizationService localizationService;
 
-    public HandleVideosCommandHandler(ILogger<HandleVideosCommandHandler> logger)
+    public HandleVideosCommandHandler(
+        ILogger<HandleVideosCommandHandler> logger,
+        ICommandDispatcher commandDispatcher,
+        ILocalizationService localizationService)
     {
         this.logger = logger;
+        this.commandDispatcher = commandDispatcher;
+        this.localizationService = localizationService;
     }
 
-    public Task HandleAsync(HandleVideosCommand command, CancellationToken cancellation = default)
+    public async Task HandleAsync(HandleVideosCommand command, CancellationToken cancellation = default)
     {
-        return Task.CompletedTask;
+        var text = await localizationService.GetTranslateByInterface("app.file.noSupportContent", command.UserId);
+        var commandTelegram = new SendMessageCommand(command.ChatId, text, parseMode: ParseMode.Html, replyToMessageId: command.MessageId);
+        await commandDispatcher.DispatchAsync(commandTelegram);
     }
 }
